c8c26029eacc98f03aad5321e8359359e51f087e
nikh-tail <nikhilrathor_cse_25a04035@dtu.ac.in>
2025-09-01 23:40:49 +0530

    Delete assign.py

--- assign.py
+++
@@ -1,4 +0,0 @@
-def compute_scores(civilians, pads, alpha=0.7):
-    # P_i = shape_score * urgency_score
-    # normalize distances and compute S_ij
-    # return score matrix of shape (N_civilians, N_pads)
